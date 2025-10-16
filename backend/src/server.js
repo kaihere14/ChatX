44940c3ae040fc3aa06e0dd2b865f1daedc99891
kaihere14 <yashpalsharma9950@gmail.com>
2025-10-16 22:09:23 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,25 @@
 import express from "express";
 import "dotenv/config";
-
+import path from "path";
 const app = express();
-
+const __dirname = path.resolve();
 
 import auth from "./routes/auth.route.js";
 import message from "./routes/message.route.js";
 app.use("/api/auth", auth);
 app.use("/api/message", message);
 
+//make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+  
+  
+}
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on port 5500");
+  console.log("Server is running on port ", process.env.PORT);
 });
