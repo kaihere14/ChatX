2f27621efe5c89432dc4011ec5b82524f3bc4edf
kaihere14 <yashpalsharma9950@gmail.com>
2025-10-17 14:32:02 +0530

    final fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const axiosInstamce = axios.create({
+export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3300/api"
